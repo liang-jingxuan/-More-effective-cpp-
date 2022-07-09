f864d559a393eba852f35f899b04dffb4d7d779c
liang-jingxuan <xx380784657@qq.com>
2022-07-09 13:42:43 +0800

    Myvector

--- My STL study/Myvector.cpp
+++ My STL study/Myvector.cpp
@@ -1,7 +1,44 @@
 #include"Myvector.h"
 using namespace std;
 
-template<class T, class Alloc=my_allocator>
+//public内容:对用户开放的内容
+
+template<class T,class Alloc>
+Myvector<T,Alloc>::iterator//返回值,因为这么iterator是在Myvector类内定义的,只在该类的内部使用
+Myvector<T,Alloc>::begin(){return start;}
+
+template<class T,class Alloc>
+Myvector<T,Alloc>::iterator
+Myvector<T,Alloc>::end(){return finish;}
+
+template<class T,class Alloc>
+Myvector<T,Alloc>::size_type
+Myvector<T,Alloc>::size() const {return (size_type)(finish-start);}
+
+template<class T,class Alloc>
+bool //bool不是Myvector特有的,所以不用指定
+Myvector<T,Alloc>::empty()const{ return start==finish;}
+/*
+template<class T,class Alloc>
+Myvector<T,Alloc>::reference
+Myvector<T,Alloc>::front()const{return *start;}
+
+template<class T,class Alloc>
+Myvector<T,Alloc>::reference
+Myvector<T,Alloc>::back()const{return *(finish-1);}
+*/
+template<class T,class Alloc>
+Myvector<T,Alloc>::size_type
+Myvector<T,Alloc>::capacity() const{return (size_type)(end_of_storage-start);}
+
+template<class T,class Alloc>
+Myvector<T,Alloc>::reference
+Myvector<T,Alloc>::operator[](size_type n){return *(start+n);}
+
+
+//protected内容:不对用户开放的,但可能被public函数调用实现某些功能的函数
+
+template<class T, class Alloc>
 void Myvector<T,Alloc>::insert_aux(iterator position,const T& x){
 
 }
--- My STL study/Myvector.h
+++ My STL study/Myvector.h
@@ -1,6 +1,6 @@
 #ifndef MY_VECTOR_H
 #define MY_VECTOR_H
-
+//来自其他文件的内容:1.my_allocator类（分配&释放内存）,2.全局的construct、destroy（构造和析构）
 template<class T, class Alloc=my_allocator>
 class Myvector{
     public:
@@ -19,42 +19,43 @@ class Myvector{
         iterator finish;
         iterator end_of_storage;
     
-    void insert_aux(iterator position,const T& x);//在position位置插入一个x
-    void deallocate();//释放掉目前的vector,一般用在push_back后空间用完,换新地址时释放掉旧的vector
-    void fill_initialize(size_type n,const T& val);
+        void insert_aux(iterator position,const T& x);//在position位置插入一个x
+        void deallocate();//释放掉目前的vector,一般用在push_back后空间用完,换新地址时释放掉旧的vector
+        void fill_initialize(size_type n,const T& val);
     public:
-    iterator begin();
-    iterator end();
-    size_type size()const;//两个指针相减得到的是ptrdiff_t,因此要转成size_type
-    size_type capacity()const;//查看当前vector还能存入多少个元素,
-    bool empty()const ;
-    reference operator[](size_type n);
+        iterator begin();
+        iterator end();
+        size_type size()const;//两个指针相减得到的是ptrdiff_t,因此要转成size_type
+        size_type capacity()const;//查看当前vector还能存入多少个元素,
+        bool empty()const ;
+        reference operator[](size_type n);
 
-    //构造函数
-    vector():start(0),finish(0),end_of_storage(0){}//无参数时构成一个0大小的vector
-    vector(size_type n,const T& val){fill_initialize(n,val);}
-    vector(int n,const T&val){fill_initialize(n,val);}
-    vector(long n,const T&val){fill_initialize(n,val);}
-    explicit vector(size_type n){fill_initialize(n,T());}
+        //构造函数
+        vector():start(0),finish(0),end_of_storage(0){}//无参数时构成一个0大小的vector
+        vector(size_type n,const T& val){fill_initialize(n,val);}
+        vector(int n,const T&val){fill_initialize(n,val);}
+        vector(long n,const T&val){fill_initialize(n,val);}
+        explicit vector(size_type n){fill_initialize(n,T());}
 
-    //析构
-    ~vector(){
-        destroy(start,finish);//全局函数,用来析构start到finish之间的对象
-        deallocate();
-    }
+        //析构
+        ~vector(){
+            destroy(start,finish);//全局函数,用来析构start到finish之间的对象
+            deallocate();
+        }
 
-    reference front();
-    reference back();
+    
+        reference back()const{return *start;}
+        reference front()const{return *(finish-1);}
 
-    void push_back(const T& x);
+        void push_back(const T& x);
 
-    void pop_back();
+        void pop_back();
 
-    iterator erase(iterator position);//消除position上的元素,后面的补上
-    iterator erase(iterator first,iterator last);//消除fist到last中间的元素,后面的补上
-    void resize(size_type new_size,const T& x);
-    void resize(size_type new_size);
-    void clear();
+        iterator erase(iterator position);//消除position上的元素,后面的补上
+        iterator erase(iterator first,iterator last);//消除fist到last中间的元素,后面的补上
+        void resize(size_type new_size,const T& x);
+        void resize(size_type new_size);
+        void clear();
 
     protected:
         iterator allocate_and_fill(size_t,const T&);
