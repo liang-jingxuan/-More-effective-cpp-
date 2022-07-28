dc6320825881e64bdedbc998b213b44487ebb352
liang-jingxuan <xx380784657@qq.com>
2022-07-28 09:49:27 +0800

    RBT的后继和前驱

--- My STL study/MY_RBT.hpp
+++ My STL study/MY_RBT.hpp
@@ -37,6 +37,7 @@ struct __rbt_node :public __rbtree_node_base{//一个节点的数据段
 };
 
 struct rbt_base_iterator{
+    //important:iterator可能指向树种任意一个非nullptr节点,当iterator为null时,说明指向了header,实现了左闭右开
     typedef __rbtree_node_base::base_ptr    base_ptr;
                                         //__rbtree_node_base*类型
     typedef bidirectional_iterator_tag      iterator_category;
@@ -44,7 +45,7 @@ struct rbt_base_iterator{
 
     base_ptr node;
 
-    void increment(){//用来找到后继
+    void increment(){//后继
         if( node->right != nullptr){//有右孩子,则后继在右子树
             node=node->right;
             while(node->left!=nullptr) node=node->left;
@@ -52,8 +53,32 @@ struct rbt_base_iterator{
         else{//没有右孩子,则该节点必然在某个节点的左子树
             while(node->parent != nullptr && node->parent->right == node) 
                 node = node->parent;
-            if(node->parent != nullptr) node = node->parent;
+            if(node->right != node) 
+                node = node->parent;
         }
+    }
+
+    void decrement(){//前驱
+    //1.考虑node是header,说明指向了end(),则前驱即最大的数,header->right即最大的数
+        if(node->color = __rbtree_node_red && node->parent->parent == node){
+            //header节点的父节点指向header,左孩子指向最小的数,右孩子指向最大的数,header颜色为红色
+            node=node->right;
+            return;
+        }
+    //2.不是header
+        if(node ->left != nullptr){
+            //有左孩子
+            node=node->left;
+            while(node->right!=nullptr) node=node->right;
+        }
+        else{
+            //无左孩子
+            while(node->parent!=nullptr&&node->parent->left==node) 
+                node=node->parent;
+            if(node->left!=node)//如果不是到达了header
+                node=node->parent;
+        }
+
     }
 };
 
