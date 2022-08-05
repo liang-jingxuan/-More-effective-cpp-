cbe7919aadbd47c41a04add40e81c9a7cbd4d3a1
liang-jingxuan <xx380784657@qq.com>
2022-08-05 15:34:36 +0800

    tst

--- My STL study/tst.cpp
+++ My STL study/tst.cpp
@@ -241,17 +241,22 @@ int Mydequetest(){
 
 
 void Myrbttest(){
-    RBT<int,int,identity<int>,less<int>> itree;//less <返回true,>=返回false
+    mySTL::RBT<int,int,identity<int>,less<int>> itree;//less <返回true,>=返回false
                                             //identity是如何根据value值求key
     itree.insert_equal(1);
-    itree.insert_equal(1);
-    itree.insert_equal(1);
-    itree.insert_unique(1);
-    itree.insert_unique(3);
-    itree.insert_unique(-10);
-    for(RBT<int,int,identity<int>,less<int>>::iterator pdq=itree.begin();
+    itree.insert_equal(2);
+    itree.insert_equal(3);
+    itree.insert_equal(4);
+    itree.insert_unique(5);
+    itree.insert_unique(6);
+    for(mySTL::RBT<int,int,identity<int>,less<int>>::iterator pdq=itree.begin();
             pdq!=itree.end();++pdq)
         std::cout<<*pdq<<',';
+    std::cout<<"\n erase test!:"<<endl;
+    itree.erase(++(++itree.begin()));
+    for(mySTL::RBT<int,int,identity<int>,less<int>>::iterator pdq=itree.begin();
+        pdq!=itree.end();++pdq)
+    std::cout<<*pdq<<',';
 }
 
 int main(){
